305efae9794e0a879fe7fe974daec808b7e0cb7c
emadnky <36477033+emadnky@users.noreply.github.com>
2018-04-27 11:33:51 +0200

    added empty serviceImpl

---
+++ src/main/java/com/sellics/service/ScoreServiceImpl.java
@@ -0,0 +1,11 @@
+package com.sellics.service;
+
+import com.sellics.datatransferobject.ScoreDTO;
+
+public class ScoreServiceImpl implements ScoreService  {
+
+    @Override
+    public ScoreDTO getScore(String keyword) {
+        return null;
+    }
+}
