637ba1a737b7e1b18b7c37557d5cb014958dbda8
emadnky <36477033+emadnky@users.noreply.github.com>
2018-04-27 11:46:13 +0200

    added ScoreController

---
+++ src/main/java/com/sellics/controller/ScoreController.java
@@ -0,0 +1,25 @@
+package com.sellics.controller;
+
+import com.sellics.datatransferobject.ScoreDTO;
+import com.sellics.service.ScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/score")
+public class ScoreController {
+
+    private final ScoreService scoreService;
+
+    public ScoreController(ScoreService scoreService) {
+        this.scoreService = scoreService;
+    }
+
+    @GetMapping("/{keyword}")
+    public ScoreDTO getScore(@PathVariable String keyword){
+        return scoreService.getScore(keyword);
+    }
+}
--- src/main/java/com/sellics/service/ScoreServiceImpl.java
+++ src/main/java/com/sellics/service/ScoreServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sellics.service;
 
 import com.sellics.datatransferobject.ScoreDTO;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ScoreServiceImpl implements ScoreService  {
 
     @Override
