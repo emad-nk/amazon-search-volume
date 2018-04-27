07880ccbdcc44e76ac22bf943f32262d6fe9f4f4
emadnky <36477033+emadnky@users.noreply.github.com>
2018-04-27 09:06:03 +0200

    initialized the project

---
+++ src/main/java/com/sellics/AmazonsearchvolumeApplication.java
@@ -0,0 +1,12 @@
+package com.sellics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonsearchvolumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonsearchvolumeApplication.class, args);
+    }
+}
